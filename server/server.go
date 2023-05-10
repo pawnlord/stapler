1adc0d2b81f0c9af858638d3f69e892c89068d32
phlpf <phil.j.p.frey@gmail.com>
2023-05-09 23:41:26 -0400

    Add structs that represent Server and Client

--- server/server.go
+++ server/server.go
@@ -12,32 +12,47 @@ const (
 	SERVER_TYPE = "tcp"
 )
 
+type Server struct {
+	listener net.Listener
+	clients  map[string]net.Conn
+}
+
+type Client struct {
+	conn       net.Conn
+	p2p_client chan net.Conn
+}
+
 func main() {
+	server := Server{}
 	fmt.Println("Server Running...")
-	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	listener, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	server.listener = listener
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
-	defer server.Close()
+	defer server.listener.Close()
 	fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
 	for {
-		connection, err := server.Accept()
+		connection, err := server.listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
 		fmt.Println("client connected")
-		go processClient(connection)
+		client := Client{connection, make(chan net.Conn)}
+		go server.processClient(client)
 	}
 }
-func processClient(connection net.Conn) {
+func (Server) processClient(client Client) {
 	buffer := make([]byte, 1024)
-	mLen, err := connection.Read(buffer)
+	mLen, err := client.conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
 	fmt.Println("Received: ", string(buffer[:mLen]))
-	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
-	connection.Close()
+
+	_, err = client.conn.Write([]byte("test"))
+
+	client.conn.Close()
 }
